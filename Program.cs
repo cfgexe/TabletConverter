378cc15710d8f6f5f9ee224a978e94039d6fb9e4
cfgexe <65036495+cfgexe@users.noreply.github.com>
2022-07-24 01:48:12 +1000

    LOLOLOLOLOLO FINISHED FINALLY

--- Program.cs
+++ Program.cs
@@ -11,7 +11,19 @@ namespace WacomArea {
         static Dictionary<string, float> TabletArea = new Dictionary<string, float>();
         static Dictionary<string, int> WacomArea = new Dictionary<string, int>();
         static public void Main(string[] args) {
-            Console.WriteLine("Wacom Area Converter made by Fysix");
+            Console.WriteLine("Would you like to convert Wacom to Hawku area or Hawku to Wacom area?");
+			Console.WriteLine("[1] Wacom To Hawku area");
+			Console.WriteLine("[2] Hawku to Wacom area");
+			int choice = int.Parse(Console.ReadLine());
+			if (choice == 1) {
+				WacToHaw();
+			}
+			else if (choice == 2) {
+				HawToWac();
+			}
+        }
+		static public void HawToWac() {
+			Console.WriteLine("Wacom Area Converter made by Fysix");
             Console.WriteLine("What is your area width?: ");
             float w = float.Parse(Console.ReadLine());
             Console.WriteLine("What is your area height?: ");
@@ -32,6 +44,36 @@ namespace WacomArea {
             Console.WriteLine("Right: " + Math.Round(wacomRight));
             Console.WriteLine("Top: " + Math.Round(wacomTop));
             Console.WriteLine("Bottom: " + Math.Round(wacomBottom));
-        } 
+			Console.ReadKey();
+		}
+		static public void WacToHaw() {
+			double ConversionFactor = 15200.0 / 152.0;
+			Console.WriteLine("Wacom Area Converter made by Fysix");
+            Console.WriteLine("What is your area Left?: ");
+            float l = float.Parse(Console.ReadLine());
+            Console.WriteLine("What is your area Right?: ");
+            float r = float.Parse(Console.ReadLine());
+            Console.WriteLine("What is your area Top?: ");
+            float t = float.Parse(Console.ReadLine());
+            Console.WriteLine("What is your area Bottom: ");
+            float b = float.Parse(Console.ReadLine());
+            TabletArea.Add("top", t);
+            TabletArea.Add("left", l);
+            TabletArea.Add("right", r);
+            TabletArea.Add("bottom", b);
+            double hawkuWidth = (r - l) / ConversionFactor;
+			hawkuWidth = Math.Round((double)hawkuWidth, 2);
+            double hawkuHeight = (b - t) / ConversionFactor;
+			hawkuHeight = Math.Round((double)hawkuHeight, 2);
+            double hawkuX = (hawkuWidth / 2) + (l / ConversionFactor);
+			hawkuX = Math.Round((double)hawkuX, 2);
+            double hawkuY = (hawkuHeight / 2) + (t / ConversionFactor);
+			hawkuY = Math.Round((double)hawkuY, 2);
+            Console.WriteLine("Width: " + hawkuWidth);
+            Console.WriteLine("Height: " + hawkuHeight);
+            Console.WriteLine("X: " + hawkuX);
+            Console.WriteLine("Y: " + hawkuY);
+			Console.ReadKey();
+		}
     }
 }
